9563b275d46e953c8406d6b6fde2f47191b13f2c
scao <sihao.cao@gmail.com>
2019-12-08 14:57:25 -0500

    added folders and notes routes

--- src/app.js
+++ src/app.js
@@ -4,16 +4,19 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const foldersRouter = require('./folders/folders-router');
+const notesRouter = require('./notes/notes-router');
 
 const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
-
-app.use(morgan(morganOption));
-app.use(helmet());
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+    skip: () => NODE_ENV === 'test'
+}))
 app.use(cors());
+app.use(helmet());
+
+app.use('/api/folders', foldersRouter)
+app.use('/api/notes', notesRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
